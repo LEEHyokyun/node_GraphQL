56d5ac7a3cfa7290fab7123f53f2ce6f0b94b039
hyokyun <gyrbs22@gmail.com>
2021-06-09 00:44:55 +0900

    Query types build and define by follwed logic_Data Create and Read

--- index.js
+++ index.js
@@ -3,17 +3,45 @@ const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
 
 //Query 작성
+//schema를 구성하는 data 요소들을 작성해준다
+//type Query data 이름{data}
+//! = 필수필드
 const schema = buildSchema(`
+
+    type Product{
+        id : ID!
+        name : String
+        price : Int
+        description : String
+    }
+
     type Query{
-        hello : String,
-        today : Int
+        getProduct (id : ID! ) : Product
     }
 `);
 
+//임의의 data
+const products = [
+  {
+    id: 1,
+    name: "첫번째 제품",
+    price: 2000,
+    description: "포도",
+  },
+  {
+    id: 2,
+    name: "두번째 제품",
+    price: 4000,
+    description: "사과",
+  },
+];
+
 //response
 const root = {
-  hello: () => "hello world",
-  today: () => 0609,
+  //getProduct, 인자를 id로 받아온다
+  //해당 인자와 id값이 일치하는 product data 응답
+  getProduct: ({ id }) =>
+    products.find((product) => product.id === parseInt(id)),
 };
 
 //express server 작동
