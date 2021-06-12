21c4c35218631c5c3a25d9ca581d4c059685a248
hyokyun <gyrbs22@gmail.com>
2021-06-12 23:53:47 +0900

    graphql data update by Mutation

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const { buildSchema } = require("graphql");
 //! = 필수필드
 const schema = buildSchema(`
 
+    type ProductInput{
+      name : String
+      price : Int
+      description : String
+    }
+
     type Product{
         id : ID!
         name : String
@@ -16,8 +22,13 @@ const schema = buildSchema(`
     }
 
     type Query{
-        getProduct (id : ID! ) : Product
+        getProduct (id : ID! ) : Product  
+      }
+
+    type Mutation{
+      addProduct( input : ProductInput ) : Product
     }
+    
 `);
 
 //임의의 data
@@ -36,12 +47,27 @@ const products = [
   },
 ];
 
+//ProductInput data
+const ProductInput = {
+  name: "첫번째 제품",
+  price: "2000",
+  description: "참외",
+};
+
 //response
 const root = {
   //getProduct, 인자를 id로 받아온다
   //해당 인자와 id값이 일치하는 product data 응답
   getProduct: ({ id }) =>
     products.find((product) => product.id === parseInt(id)),
+
+  addProduct: ({ input }) => {
+    //data를 추가하면서, input한 data 항목에 id 추가
+    input.id = parseInt(product.length + 1);
+    products.push(input);
+
+    return root.getProduct({ id: input.id });
+  },
 };
 
 //express server 작동
